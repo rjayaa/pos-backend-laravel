2ce7925c78cc34d4ec0bd1aabc317094ef616133
rjayaa <rjaya.lelana03@gmail.com>
2024-02-15 21:41:14 +0700

    pos bootcamp session 1 and 2, configuration laravel and implement crud for user section

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/home',function(){
+        return view('pages.dashboard');
+    })->name('home');
+    Route::resource('users', UserController::class);
+});
+
+
